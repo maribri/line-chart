405b10abe0331a08f3dc1a8efd796bbb77cf1f2e
maribri <marina.brianova@gmail.com>
2025-11-26 05:23:54 -0800

    Fix config

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,20 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    svgr({
-      svgrOptions: {
-        icon: true,
-      },
-    }),
-  ],
-  css: {
-    modules: {
-      generateScopedName: '[local]_[hash:base64:5]',
+    base: '/line-chart/',
+    plugins: [
+        react(),
+        svgr({
+            svgrOptions: {
+                icon: true,
+            },
+        }),
+    ],
+    css: {
+        modules: {
+            generateScopedName: '[local]_[hash:base64:5]',
+        },
     },
-  },
 })
